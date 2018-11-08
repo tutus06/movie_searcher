c875cd51c2d0fd885365e3d37ee5780a451f7068
tutus06 <arthurmansuy@hotmail.fr>
2018-11-09 00:01:53 +0100

    last commit

--- app/services/search_movie.rb
+++ app/services/search_movie.rb
@@ -1,8 +1,10 @@
+Dotenv.load
+
 class SearchMovie
 
   def initialize(movie)
     @movie = movie
-		Tmdb::Api.key(Rails.application.credentials[:themovieapi])
+		Tmdb::Api.key(ENV['YOUR_MOVIEDB_API_KEY_HERE'])
   end
 
   def result
